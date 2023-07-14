3d0019c0976254f661d02296f18a5e2e55c9d980
Shegzzy <miksegun1996@gmail.com>
2023-07-14 19:39:16 +0100

    updated code

--- Studybud/settings.py
+++ Studybud/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_URL = "/staticfiles/"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'base/static'
